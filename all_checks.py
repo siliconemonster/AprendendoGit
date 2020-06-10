b80ec14404fb9c40fac7f46095691c295f6c8667
aline <alinefrez@netscape.net>
2020-06-10 20:11:04 -0300

    Fixing typo

--- all_checks.py
+++ all_checks.py
@@ -8,7 +8,7 @@ def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 def check_disk_full(disk, min_gb, min_percent):
-    """Returns True if there isn't rnough disk space, False otherwise."""
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total

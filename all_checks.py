7e3292d4bc0f71b20b8b5ff8d1e3a6a082a8eafe
aline <alinefrez@netscape.net>
2020-06-10 20:59:19 -0300

    Added CPU check

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import psutil
 import shutil
 import sys
 
@@ -22,10 +23,15 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk = "/", min_gb = 2, min_percent = 10)
 
+def check_cpu_constrained():
+    """Returns True if the CPU is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
+
 def main():
     checks = [
         (check_reboot, "Pending Reboot."),
         (check_root_full, "Root Partition Full."),
+        (check_cpu_constrained, "CPU load too high."),
     ]
     everything_ok = True
     for check, msg in checks:
